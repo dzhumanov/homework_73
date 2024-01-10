f9c1d761b44f7bbe835429162d650056671b934e
Temirlan <temadjumanov@gmail.com>
2024-01-10 23:24:44 +0600

    installed Vigenere cipher. Completed task 2.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get('/:content', (req, res) => {
-    res.send("Your message: " + req.params.content);
+const Vigenere = require("caesar-salad").Vigenere;
+const password = "password";
+
+app.get("/:content", (req, res) => {
+  res.send("Your message: " + req.params.content);
+});
+
+app.get("/encode/:text", (req, res) => {
+  const codedText = Vigenere.Cipher(password).crypt(req.params.text);
+  res.send("Your message was coded to: " + codedText);
+});
+
+app.get("/decode/:text", (req, res) => {
+  const uncodedText = Vigenere.Decipher(password).crypt(req.params.text);
+  res.send("Your message was decoded. Original text: " + uncodedText);
 });
 
 app.listen(port, () => {
-    console.log('Server online on ' + port);
-})
-
+  console.log("Server online on " + port);
+});
